aa4057fbc6b13a763e6b80efca741c64ad204f25
eduhenke <eduardo_henke@hotmail.com>
2022-03-10 00:21:59 -0300

    Add back Type Show implementation for recursive types

--- simply-typed-with-recursive-types/app/Type.hs
+++ simply-typed-with-recursive-types/app/Type.hs
@@ -13,21 +13,21 @@ data Type
   | TyTop
   | TyVar Int Int
   | TyRecursive String Type
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
 
--- instance Show Type where
---   show =
---     let show' ctx ty = case ty of
---           TyBool -> "Bool"
---           TyNat -> "Nat"
---           (TyArrow ty1 ty2) -> show' ctx ty1 ++ "->" ++ show' ctx ty2
---           TyUnit -> "Unit"
---           (TyRecord ts) ->
---             if keys ts == map show [1 .. (length $ keys ts)]
---               then "{" ++ intercalate ", " (map (\(l, t) -> show' ctx t) $ toList ts) ++ "}"
---               else "{" ++ intercalate ", " (map (\(l, t) -> l ++ ":" ++ show' ctx t) $ toList ts) ++ "}"
---           (TyVariant ts) -> "<" ++ intercalate ", " (map (\(l, t) -> l ++ ":" ++ show' ctx t) $ toList ts) ++ ">"
---           TyTop -> "Top"
---           (TyVar n _) -> if n < length ctx then ctx !! n else "<TyVar ???>"
---           (TyRecursive x body) -> "(µ" ++ x ++ "." ++ show' (x : ctx) body ++ ")"
---      in show' []
+instance Show Type where
+  show =
+    let show' ctx ty = case ty of
+          TyBool -> "Bool"
+          TyNat -> "Nat"
+          (TyArrow ty1 ty2) -> show' ctx ty1 ++ "->" ++ show' ctx ty2
+          TyUnit -> "Unit"
+          (TyRecord ts) ->
+            if keys ts == map show [1 .. (length $ keys ts)]
+              then "{" ++ intercalate ", " (map (\(l, t) -> show' ctx t) $ toList ts) ++ "}"
+              else "{" ++ intercalate ", " (map (\(l, t) -> l ++ ":" ++ show' ctx t) $ toList ts) ++ "}"
+          (TyVariant ts) -> "<" ++ intercalate ", " (map (\(l, t) -> l ++ ":" ++ show' ctx t) $ toList ts) ++ ">"
+          TyTop -> "Top"
+          (TyVar n _) -> if n < length ctx then ctx !! n else "<TyVar ???>"
+          (TyRecursive x body) -> "(µ" ++ x ++ "." ++ show' (x : ctx) body ++ ")"
+     in show' []
